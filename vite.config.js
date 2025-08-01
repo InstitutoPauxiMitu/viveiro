84fa68b7a2faa1ed5011fc69fdba5558e352dd35
Lucas Spavier <instituto.pauxi.mitu.app@gmail.com>
2025-08-01 09:12:59 -0300

    alteração do vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Define o caminho base como relativo
-});
+  // Adicione a base da URL para a sua aplicação
+  base: '/', 
+  build: {
+    // A pasta de saída do build
+    outDir: 'dist',
+  }
+})
